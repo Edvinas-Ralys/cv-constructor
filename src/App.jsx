d9de451f9e3ad1abb044865ce73852cb2418cd74
Edvinas-Ralys <edvinas.ralys97@gmail.com>
2024-01-15 17:38:21 +0200

    Reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,15 @@
 import "./assets/style/App.scss";
 import "./assets/style/reset.scss";
 import "./assets/style/button.scss";
-import Preview from "./assets/components/Preview";
-import Input from "./assets/components/Input";
-import { useState } from "react";
+
 
 function App() {
-  const [basicMultiText, setBasicMultiText] = useState([``, ``, ``, ``, ``, ``, ``])
-  const handleMultiText = (e, index) =>{
-    setBasicMultiText(prev => prev.map((item, i) => i === index ? e.target.value : item))
-  }
+
 
   return (
     <div className="main">
 
-      <div className="sides">
-        <Input
-          basicMultiText={basicMultiText}
-          handleMultiText={handleMultiText}
-        />
 
-        <Preview
-          basicMultiText={basicMultiText}
-
-        />
-      </div>
     </div>
   );
 }
--- src/assets/components/input/EduInfoStaging.jsx
+++
@@ -1,4 +0,0 @@
-let EduInfo = []
-
-
-export default EduInfo
