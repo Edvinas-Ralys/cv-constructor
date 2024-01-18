64af92459ef3c0fded3477c0e2f66fd5c1865aff
Edvinas-Ralys <edvinas.ralys97@gmail.com>
2024-01-18 15:40:24 +0200

    Added alnguages

--- src/assets/components/LanguageItem.jsx
+++ src/assets/components/LanguageItem.jsx
@@ -1,24 +1,45 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { DeleteIcon } from "./Icons"
 import { CVInformation } from "./data/InformationCont"
 
 function LanguageItem({ lang }) {
+  const { setLanguages } = useContext(CVInformation)
+  const [editCard, setEditCard] = useState(false)
 
-    const {setLanguages} = useContext(CVInformation)
-
-    const deleteLang = _ =>{
-        setLanguages(prev => prev.filter(langInArr => langInArr.id !== lang.id))
-    }
-
+  const deleteLang = _ => {
+    setLanguages(prev => prev.filter(langInArr => langInArr.id !== lang.id))
+  }
 
   return (
     <div className="lang-card">
       <div className="cont">
-        <div className="cont-title">{lang.text === `` ? `Not specified` : lang.text}</div>
+        <div onClick={_ => setEditCard(!editCard)} className="cont-title">
+          {lang.text === `` ? `Not specified` : lang.text}
+        </div>
         <div onClick={deleteLang} className="delete">
           {DeleteIcon}
         </div>
       </div>
+      {editCard && (
+        <div className="lang-edit">
+          <div className="section">
+            <div className="input">
+              <label htmlFor="language">Language</label>
+              <input
+                type="text"
+                value={lang.text}
+                onChange={e =>
+                  setLanguages(langsInArr =>
+                    langsInArr.map(langInArr =>
+                      langInArr.id === lang.id ? { ...langInArr, text: e.target.value } : langInArr
+                    )
+                  )
+                }
+              />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
